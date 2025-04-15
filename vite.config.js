b6c843cf3399cfcff4ec0debd7457a3a1eb7f668
yohanmanohara <warushayohan80@gmail.com>
2025-04-16 02:07:40 +0530

    make Progressive Web App

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,30 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
     sveltekit(),
-  ],
+    SvelteKitPWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Expense Tracker',
+        short_name: 'Expenses',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#4caf50',
+        icons: [
+          
+          {
+            src: '/logo.jpg',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 });
